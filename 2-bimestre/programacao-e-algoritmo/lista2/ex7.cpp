80f0a822f9d721e19a6ff5bbcde7365c20d885bd
Rafael Dantas <Rd4273051@gmail.com>
2026-06-18 16:07:49 -0700

    Update ex7.cpp

--- 2-bimestre/programacao-e-algoritmo/lista2/ex7.cpp
+++ 2-bimestre/programacao-e-algoritmo/lista2/ex7.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Função que analisa a ordem dos números
+
 string verificarOrdem(int n1, int n2, int n3, int n4, int n5) {
 
     if (n1 < n2 && n2 < n3 && n3 < n4 && n4 < n5) {
