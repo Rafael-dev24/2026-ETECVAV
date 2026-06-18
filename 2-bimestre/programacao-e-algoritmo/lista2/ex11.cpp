cbafe7449f78090b69606260114492f08bf111e0
Rafael Dantas <Rd4273051@gmail.com>
2026-06-18 16:02:16 -0700

    Clean up comments and formatting in ex11.cpp
    Removed comments for clarity and improved formatting.

--- 2-bimestre/programacao-e-algoritmo/lista2/ex11.cpp
+++ 2-bimestre/programacao-e-algoritmo/lista2/ex11.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Função que substitui números negativos por zero
+
 void substituirNegativos(int vetor[], int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         if (vetor[i] < 0) {
@@ -13,16 +13,16 @@ void substituirNegativos(int vetor[], int tamanho) {
 int main() {
     int vetor[10];
 
-    // Leitura dos 10 números
+    
     for (int i = 0; i < 10; i++) {
         cout << "Digite o numero " << i + 1 << ": ";
         cin >> vetor[i];
     }
 
-    // Substitui os negativos por zero
+    
     substituirNegativos(vetor, 10);
 
-    // Exibe o vetor
+    
     cout << "\nVetor resultante:\n";
 
     for (int i = 0; i < 10; i++) {
