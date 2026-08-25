38462de92bf22e0b64d37c58f1671e851eeab087
Rafael Dantas <Rd4273051@gmail.com>
2026-08-25 12:49:10 -0700

    Delete 3-bimestre directory

--- 3-bimestre/programacao_e_algoritmos/exercicio1.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("digite um número: "))
-
-if numero < 0:
-    print(f"{numero} é negativo!")
-elif numero > 0:
-    print(f"{numero} é positivo!")
-else:
-    print(f"{numero} é invalido!")
