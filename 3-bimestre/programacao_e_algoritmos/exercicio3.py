9ac4fca272d7b0f96e95049091302d5bdeaf61e4
Rafael Dantas <Rd4273051@gmail.com>
2026-08-25 13:06:29 -0700

    Delete 3-bimestre directory

--- 3-bimestre/programacao_e_algoritmos/exercicio3.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("digite um número: "))
-contador = 0
-
-if numero > 0:
-    while contador <= numero:
-        print(f"{contador}")
-        contador += 1
-        
--- 3-bimestre/programacao_e_algoritmos/exercicio8.py
+++
@@ -1,6 +0,0 @@
-n = int(input('digite um número: '))
-
-if n % 2 !=0:
-    print('É um número primo!')
-else:
-    print('Não é um número primo!')
